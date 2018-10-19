dda74fc221f0b59b704cea01752cfbdf529dd5c6
Tom <Thomas.Bekman12@ncf.edu>
2018-10-19 13:05:00 -0600

    Working server_2.py

--- networking/server_2.py
+++ networking/server_2.py
@@ -1,78 +1,28 @@
-import socket
-import sys
-import traceback
-from threading import Thread
+import socket                   # Import socket module
 
+port = 60002                    # Reserve a port for your service.
+s = socket.socket()             # Create a socket object
+host = socket.gethostname()     # Get local machine name
+s.bind((host, port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
 
-def main():
-    start_server()
+print ('Server listening....')
 
+while True:
+    conn, addr = s.accept()     # Establish connection with client.
+    print ('Got connection from', addr)
+    data = conn.recv(1024)
+    print('Server received', repr(data))
 
-def start_server():
-    host = "127.0.0.1"
-    port = 65432        # arbitrary non-privileged port
+    filename='mytext.txt'
+    f = open(filename,'rb')
+    l = f.read(1024)
+    while (l):
+       conn.send(l)
+       print('Sent ',repr(l))
+       l = f.read(1024)
+    f.close()
 
-    soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)   # SO_REUSEADDR flag tells the kernel to reuse a local socket in TIME_WAIT state, without waiting for its natural timeout to expire
-    print("Socket created")
-
-    try:
-        soc.bind((host, port))
-    except:
-        print("Bind failed. Error : " + str(sys.exc_info()))
-        sys.exit()
-
-    soc.listen(5)       # queue up to 5 requests
-    print("Socket now listening")
-
-    # infinite loop- do not reset for every requests
-    while True:
-        connection, address = soc.accept()
-        ip, port = str(address[0]), str(address[1])
-        print("Connected with " + ip + ":" + port)
-
-        try:
-            Thread(target=client_thread, args=(connection, ip, port)).start()
-        except:
-            print("Thread did not start.")
-            traceback.print_exc()
-
-    soc.close()
-
-
-def client_thread(connection, ip, port, max_buffer_size = 100000):
-    is_active = True
-
-    while is_active:
-        client_input = receive_input(connection, max_buffer_size)
-
-        if "--QUIT--" in client_input:
-            print("Client is requesting to quit")
-            connection.close()
-            print("Connection " + ip + ":" + port + " closed")
-            is_active = False
-        else:
-            print("Processed result: {}".format(client_input))
-            connection.sendall("-".encode("utf8"))
-
-
-def receive_input(connection, max_buffer_size):
-    client_input = connection.recv(max_buffer_size)
-    client_input_size = sys.getsizeof(client_input)
-
-    if client_input_size > max_buffer_size:
-        print("The input size is greater than expected {}".format(client_input_size))
-
-    decoded_input = client_input.decode("utf8").rstrip()  # decode and strip end of line
-    result = process_input(decoded_input)
-
-    return result
-
-
-def process_input(input_str):
-    print("Processing the input received from client")
-
-    return "Hello " + str(input_str).upper()
-
-if __name__ == "__main__":
-    main()
+    print('Done sending')
+    conn.send('Thank you for connecting'.encode('utf-8'))
+    conn.close()
